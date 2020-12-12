f7ac5d45e4664000fe4ee37c0e8da6070c9346b6
mraasvel <mbraasveldt@gmail.com>
2020-12-12 16:27:21 +0100

    final program works but is very slow

--- get_data/run.sh
+++ get_data/run.sh
@@ -1,8 +1,15 @@
+bash get_data.sh
+
 make
 ./a.out 1 > files/get_note.sh 2> files/stats.txt
 ./a.out 2 > files/get_comment_one.sh
 ./a.out 3 > files/get_comment_two.sh
 
-bash files/get_note.sh
+bash files/get_note.sh >> files/note_data.txt
 bash files/get_comment_one.sh
 bash files/get_comment_two.sh
+
+cat files/comment_one_data.txt >> note_data.txt
+cat files/comment_two_data.txt >> note_data.txt
+rm files/comment_one_data.txt
+rm files/comment_two_data.txt
